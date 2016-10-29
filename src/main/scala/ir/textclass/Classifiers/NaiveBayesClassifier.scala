2ee39a2d45e0ce5b48f048741841c1fcbbb56b7e
Andrius Juodelis <juodelis@puzzle.ch>
2016-10-29 19:21:50 +0200

    for 2 labels with model as map

--- src/main/scala/ir/textclass/Classifiers/NaiveBayesClassifier.scala
+++ src/main/scala/ir/textclass/Classifiers/NaiveBayesClassifier.scala
@@ -1,35 +1,34 @@
 package ir.textclass.Classifiers
 
+import breeze.linalg.max
 import ch.ethz.dal.tinyir.io.ReutersRCVStream
 import ch.ethz.dal.tinyir.processing.{Tokenizer, XMLDocument}
 
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 
-class NaiveBayesClassifier(val trainstream: ReutersRCVStream, val code: String = "C21") extends TextClassifier {
+class NaiveBayesClassifier(val xmldocs:Stream[XMLDocument], val code:String, val vocabSize:Long, val n:Long)  {
+
 
   var length: Long = 0
   var tokens: List[String] = List()
-  var n: Integer = 0;
+
   var npos: Integer = 0;
-  val xmldocs = trainstream.stream
+  //val xmldocs = trainstream.stream
   val xmldocspos = xmldocs.filter(_.codes(code))
   val xmldocsneg = xmldocs.filter(!_.codes(code))
 
   /*Calculate p(c) for the two classes and store it in a map (perhaps there are better structures)*/
   var pc = Map("cpos" -> 1.0, "cneg" -> 0.0)
-  n = trainstream.length
   npos = xmldocspos.length
   pc = pc + ("cpos" -> npos.toFloat / n)
   pc = pc + ("cneg" -> (1 - npos.toFloat / n))
-  print(pc)
+  //print(pc)
 
   /* Calculate and store the p(w|c) for the two classes and store the values in pwcpos and pwcnegwith alpha=1 smoothing
   * pwcpos map contains only tokens of the poitivedocuments
   *
    */
-  val vocab = xmldocs.flatMap(_.tokens).distinct
-  val vocabSize = xmldocs.flatMap(_.tokens).distinct.length
 
   //denominator
   val sumlengthdpos = xmldocspos.flatMap(_.tokens).length.toDouble + vocabSize
@@ -40,8 +39,8 @@ class NaiveBayesClassifier(val trainstream: ReutersRCVStream, val code: String =
   val pwcneg = xmldocsneg.flatMap(_.tokens).groupBy(identity).mapValues(_.size + 1)
 
 
-  println(pwcpos)
-  println(pwcneg.map(a => a._2).sum)
+ // println(pwcpos)
+ // println(pwcneg.values.sum)
 
   /*Returns the two class probabilities. The bigger one should be picked if only interested in the class*/
   def prediction(xmlDoc: XMLDocument): ((String, Double), (String, Double)) = {
@@ -49,13 +48,15 @@ class NaiveBayesClassifier(val trainstream: ReutersRCVStream, val code: String =
 
     var cpos = scala.math.log(pc("cpos"))
     var cneg = scala.math.log(pc("cneg"))
-    print(xmlDoc.content)
+    //print(xmlDoc.content)
     /*Calculate likelihood of d for pos */
 
     cpos = Tokenizer.tokenize(xmlDoc.content).map(tkn => scala.math.log(pwcpos.getOrElse(tkn, 1) / sumlengthdpos)).sum
     cpos = Tokenizer.tokenize(xmlDoc.content).map(tkn => scala.math.log(pwcneg.getOrElse(tkn, 1) / sumlengthdneg)).sum
 
     (("cpos", cpos), ("cneg", cneg))
+
+    //max(cpos, cneg)
 
 
 //    if( cpos >= cneg){
@@ -73,18 +74,6 @@ class NaiveBayesClassifier(val trainstream: ReutersRCVStream, val code: String =
 //    println(" ")
 //
 //
-
-  }
-
-
-  override def classify: mutable.Map[String, ListBuffer[String]] = {
-
-    //todo copy coding for naive classifier
-    val path: String = "/Users/Ralph/Development/ETH/Information Retrieval/Project 1/zipTrain"
-    val reuters = new ReutersRCVStream(path)
-
-
-    mutable.Map[String, ListBuffer[String]]()
 
   }
 
--- src/main/scala/ir/textclass/TextCategorizationMain.scala
+++ src/main/scala/ir/textclass/TextCategorizationMain.scala
@@ -1,33 +1,47 @@
 package ir.textclass
 
 import ch.ethz.dal.tinyir.io.ReutersRCVStream
-import ch.ethz.dal.tinyir.processing.Tokenizer
 import ch.ethz.dal.tinyir.util.StopWatch
-import ir.textclass.Classifiers.{NaiveBayesClassifier, TextClassifier}
+import ir.textclass.Classifiers.NaiveBayesClassifier
+
 
 object TextCategorizationMain {
 
   def main(args: Array[String]): Unit = {
 
 
+    // TODO read samples path from agrs
+    //    if (args.isEmpty) {
+    //      val path_train: String = "/Users/Ralph/Development/ETH/Information Retrieval/Project 1/zipTrain"
+    //      //val path_test: String =
+    //    }
+    //    else {
+    //      val path_train = args(0)
+    //      val Path_test = args(1)
+    //    }
 
-//    if (args.isEmpty) {
-//      val path_train: String = "/Users/Ralph/Development/ETH/Information Retrieval/Project 1/zipTrain"
-//      //val path_test: String =
-//    }
-//    else {
-//      val path_train = args(0)
-//      val Path_test = args(1)
-//    }
+    // TODO read all dictinct labes
+    // but for now just an array
+
 
     val myStopWatch = new StopWatch()
 
     val reuters = new ReutersRCVStream("/home/ajuodelis/eth/ir/project1/train")
-    val test = new ReutersRCVStream("/home/ajuodelis/eth/ir/project1/test")
-    val bayes = new NaiveBayesClassifier(reuters)
-    bayes.prediction(test)
+    //val test = new ReutersRCVStream("/home/ajuodelis/eth/ir/project1/test")
+    val trainDoc = reuters.stream;
 
+    val vocabSize = trainDoc.flatMap(_.tokens).distinct.length
+    val n = trainDoc.length
 
+    val labels = List("M13", "C21")
+
+    var model = Map[String, NaiveBayesClassifier]()
+
+    labels.foreach(label => {
+      model += (label -> new NaiveBayesClassifier(trainDoc, label, vocabSize, n))
+    })
+
+    print(model)
 
   }
 
