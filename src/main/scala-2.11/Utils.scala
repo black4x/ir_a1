29a3d1293f18c9fc49878155086f18ffb50faaa4
Andrius Juodelis <juodelis@puzzle.ch>
2016-11-08 16:28:03 +0100

    add comment to Utils

--- src/main/scala-2.11/Utils.scala
+++ src/main/scala-2.11/Utils.scala
@@ -3,6 +3,12 @@ import ch.ethz.dal.tinyir.io.ZipDirStream
 import scala.io.Source._
 
 object Utils {
+
+  /**
+    * Creates Map with Code Definitions from zip files in given directory
+    * @param path to Dir with zip
+    * @return Map: Code -> Definition
+    */
   def getCodeValueMap(path: String): Map[String, String] =
     new ZipDirStream(path).stream.map(fileInputStream =>
       // convert each file to a map: (code -> text)
